3178b7b5b0b45b15933cde54685d74510c6797d2
ytommy <ytommyb@gmail.com>
2025-05-05 19:42:19 +0200

    funzioni query (select, insert, update)

--- WebPage/Backend/db.php
+++ WebPage/Backend/db.php
@@ -12,32 +12,89 @@ if($conn->connect_errno) {
 
 $conn->set_charset('utf8');
 
-/**  
+/**
  * @param Arg[] $args
+ * @param Arg[] $cond_args
 */
-function query_select(Table $table, array $args, array $values) {
-	$sql = 'SELECT ';
+function query_select(Table $table, array $select_args, array $cond_args, array $cond_values) {
+	global $conn;
+	
+	$param_type = '';
+	foreach($cond_args as $arg) {
+		$cond[] = $arg->value . ' = ?';
+		$param_type .= $arg->getType();
+	}
+
+	$sql = 'SELECT '. implode(', ', $select_args) . ' FROM ' . $table->value. ' ' . implode(' AND ', $cond) . ';';
+
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param($param_type, ...$cond_values);
+	$stmt->execute();
+	return $stmt->get_result();
 }
 
 /**  
  * @param Arg[] $args
 */
-function query_insert(Table $table, array $args, array $values) {
+function query_insert(Table $table, array $insert_args, array $insert_values) {
+	global $conn;
+	
+	$param_type = '';
+	foreach($insert_args as $arg) {
+		$insert_placeholder[] = '?';
+		$param_type .= $arg->getType();
+	}
 
+	$sql = 'INSERT INTO '. $table . '(' . implode(', ', $insert_args) . ') VALUES(' . implode(', ', $insert_placeholder) . ');'; 
+
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param($param_type, ...$values);
+	$stmt->execute();
+	return $stmt->get_result();
+}
+
+/**  
+ * @param Arg[] $args
+ * @param Arg[] $cond_args
+*/
+function query_update(Table $table, array $update_args, array $update_values, array $cond_args, array $cond_values) {
+	global $conn;
+	
+	$param_type = '';
+	$param_values = array_merge($update_values, $cond_values);
+	foreach($update_args as $arg) {
+		$param_type .= $arg->getType();
+	}
+	foreach($cond_args as $arg) {
+		$cond[] = $arg->value . ' = ?';
+		$param_type .= $arg->getType();
+	}
+
+	$sql = 'UPDATE '. $table . 'SET ' . implode(' = ?, ', $update_args) . ' WHERE '. implode(' AND ', $cond) . ';';
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param($param_type, ...$param_values);
+	$stmt->execute();
+	return $stmt->get_result();
 }
 
 /**  
  * @param Arg[] $args
 */
-function query_update(Table $table, array $args, array $values) {
+function query_delete(Table $table, array $select_args, array $cond_args, array $cond_values) {
+	global $conn;
+	
+	$param_type = '';
+	foreach($cond_args as $arg) {
+		$cond[] = $arg->value . ' = ?';
+		$param_type .= $arg->getType();
+	}
 
-}
+	$sql = 'SELECT '. implode(', ', $select_args) . ' FROM ' . $table->value. ' ' . implode(' AND ', $cond) . ';';
 
-/**  
- * @param Arg[] $args
-*/
-function query_delete(Table $table, array $args, array $values) {
-
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param($param_type, ...$cond_values);
+	$stmt->execute();
+	return $stmt->get_result();
 }
 
 enum Table: string{
@@ -111,61 +168,61 @@ enum Arg: string{
 
 	public function getType(): string {
 		switch ($this) {
-			case IdAdmin: return 'i';
-			case Username: return 's';
-			case HashPassword: return 's';
-			case IdUtente: return 'i';
-			case Mail: return 's';
-			case Telefono: return 's';
-			case TwoFA: return 's';
-			case Verificato: return 's';
-			case DataCreazione: return 's';
-			case VisualizzaMail: return 's';
-			case VisualizzaTelefono: return 's';
-			case IdCitta: return 'i';
-			case Cap: return 'i';
-			case Nome: return 's';
-			case Provincia: return 's';
-			case Paese: return 's';
-			case CodiceFiscale: return 's';
-			case Cognome: return 's';
-			case Sesso: return 's';
-			case DataNascita: return 's';
-			case Nazionalita: return 's';
-			case IndirizzoScolastico: return 's';
-			case Voto: return 'i';
-			case Descrizione: return 's';
-			case ResidenzaCitta: return 'i';
-			case ResidenzaVia: return 's';
-			case ResidenzaCivico: return 'i';
-			case DomicilioCitta: return 'i';
-			case DomicilioVia: return 's';
-			case DomicilioCivico: return 'i';
-			case IdAbilita: return 'i';
-			case Studente: return 'i';
-			case IVA: return 'i';
-			case Settore: return 's';
-			case NumeroDipendenti: return 'i';
-			case Link: return 's';
-			case ReferenteCodiceFiscale: return 's';
-			case ReferenteNome: return 's';
-			case ReferenteCognome: return 's';
-			case ReferenteDataNascita: return 's';
-			case IdSede: return 'i';
-			case Azienda: return 'i';
-			case Citta: return 'i';
-			case Via: return 's';
-			case Civico: return 'i';
-			case Legale: return 's';
-			case IdAnnuncio: return 'i';
-			case TipoContratto: return 's';
-			case Mansione: return 's';
-			case AreaDisciplinare: return 's';
-			case AbilitaRichieste: return 's';
-			case DataPubblicazione: return 's';
-			case DataScadenza: return 's';
-			case MaxIscrizioni: return 'i';
-			case Sede: return 'i';
+			case Arg::IdAdmin: return 'i';
+			case Arg::Username: return 's';
+			case Arg::HashPassword: return 's';
+			case Arg::IdUtente: return 'i';
+			case Arg::Mail: return 's';
+			case Arg::Telefono: return 's';
+			case Arg::TwoFA: return 's';
+			case Arg::Verificato: return 's';
+			case Arg::DataCreazione: return 's';
+			case Arg::VisualizzaMail: return 's';
+			case Arg::VisualizzaTelefono: return 's';
+			case Arg::IdCitta: return 'i';
+			case Arg::Cap: return 'i';
+			case Arg::Nome: return 's';
+			case Arg::Provincia: return 's';
+			case Arg::Paese: return 's';
+			case Arg::CodiceFiscale: return 's';
+			case Arg::Cognome: return 's';
+			case Arg::Sesso: return 's';
+			case Arg::DataNascita: return 's';
+			case Arg::Nazionalita: return 's';
+			case Arg::IndirizzoScolastico: return 's';
+			case Arg::Voto: return 'i';
+			case Arg::Descrizione: return 's';
+			case Arg::ResidenzaCitta: return 'i';
+			case Arg::ResidenzaVia: return 's';
+			case Arg::ResidenzaCivico: return 'i';
+			case Arg::DomicilioCitta: return 'i';
+			case Arg::DomicilioVia: return 's';
+			case Arg::DomicilioCivico: return 'i';
+			case Arg::IdAbilita: return 'i';
+			case Arg::Studente: return 'i';
+			case Arg::IVA: return 'i';
+			case Arg::Settore: return 's';
+			case Arg::NumeroDipendenti: return 'i';
+			case Arg::Link: return 's';
+			case Arg::ReferenteCodiceFiscale: return 's';
+			case Arg::ReferenteNome: return 's';
+			case Arg::ReferenteCognome: return 's';
+			case Arg::ReferenteDataNascita: return 's';
+			case Arg::IdSede: return 'i';
+			case Arg::Azienda: return 'i';
+			case Arg::Citta: return 'i';
+			case Arg::Via: return 's';
+			case Arg::Civico: return 'i';
+			case Arg::Legale: return 's';
+			case Arg::IdAnnuncio: return 'i';
+			case Arg::TipoContratto: return 's';
+			case Arg::Mansione: return 's';
+			case Arg::AreaDisciplinare: return 's';
+			case Arg::AbilitaRichieste: return 's';
+			case Arg::DataPubblicazione: return 's';
+			case Arg::DataScadenza: return 's';
+			case Arg::MaxIscrizioni: return 'i';
+			case Arg::Sede: return 'i';
 			default: return 's';
 		}
 	}
